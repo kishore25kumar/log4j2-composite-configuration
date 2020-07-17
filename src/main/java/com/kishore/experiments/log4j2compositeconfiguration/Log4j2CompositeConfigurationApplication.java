5dd93d4943ce536a3415da4f9e9bab7612fa5032
Kishore Battula <Kishore.Battula@microsoft.com>
2020-07-17 16:28:19 +0530

    Log4j2 composite configuration example

---
+++ src/main/java/com/kishore/experiments/log4j2compositeconfiguration/Log4j2CompositeConfigurationApplication.java
@@ -0,0 +1,17 @@
+package com.kishore.experiments.log4j2compositeconfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4j2CompositeConfigurationApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Log4j2CompositeConfigurationApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4j2CompositeConfigurationApplication.class, args);
+		LOGGER.info("Application started successfully !!");
+	}
+
+}
